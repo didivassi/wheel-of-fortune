85e150182c149384ff9b4a7c415a18f8bf337911
manuelamind <maria.dourado@mindswap.academy>
2021-06-23 14:26:30 +0100

    Create README.md

--- src/academy/mindswap/game/Game.java
+++ src/academy/mindswap/game/Game.java
@@ -1,4 +1,43 @@
 package academy.mindswap.game;
 
-public class Game {
+import academy.mindswap.player.Player;
+
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Game implements Runnable {
+
+    private static final int MAX_NUM_OF_PLAYERS = 3;
+
+    private ArrayList <Socket> listOfPlayers;
+    private int[] playersScore;
+
+    public Game() {
+
+    }
+
+    public boolean isFull() {
+        return listOfPlayers.size() == MAX_NUM_OF_PLAYERS;
+    }
+    public void addPlayers(Socket playerSocket) {
+        listOfPlayers.add(playerSocket);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    /*public String getName() {
+        return
+    }*/
+
+
+    /*START()
+    SPINWHEEL()
+    askForPhrase()
+    games starts
+    game aks server a phrase (game replace the words for #)
+    game broadcasts the phrase to all players*/
+
 }
