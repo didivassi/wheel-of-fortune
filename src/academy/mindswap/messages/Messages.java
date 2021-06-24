5650bb37f0b4b8e0de1491187f4294dfb9aec333
BastosFilipa <filipa.s.bastos@gmail.com>
2021-06-24 17:07:31 +0100

    added quotes and file.txt added methods to prepare quote to game

--- src/academy/mindswap/messages/Messages.java
+++ src/academy/mindswap/messages/Messages.java
@@ -1,7 +1,9 @@
 package academy.mindswap.messages;
 
 public class Messages {
-    public static final String DEFAULT_NAME = "Player_";
-    public static final String PLAYER_ENTERED_GAME = "Welcome to game, be prepared to win!";
-    public static final String SPIN_WHEEL = "The wheel is spinning, we wish you luck!";
+    public static final String ASK_NAME = "Write your name!";
+    public static final String PLAYER_ENTERED_GAME = "Welcome to game, be prepared to win!\n";
+    public static final String START_GAME = "The game will start!\n";
+    public static final String PERMITION_TO_TALK = "/talk now\n";
+    public static final String SPIN_WHEEL = "The wheel is spinning, we wish you luck!\n";
 }
