dd3ff24809df97b27d7cd4c4b88d6905309c97e8
BastosFilipa <filipa.s.bastos@gmail.com>
2021-06-27 14:21:22 +0100

    Added game type option to server star method and createGame method refactor of WheelOfFortune to Game

--- src/academy/mindswap/server/Server.java
+++ src/academy/mindswap/server/Server.java
@@ -10,7 +10,10 @@
 package academy.mindswap.server;
 
 import static academy.mindswap.server.Messages.*;
-import academy.mindswap.game.Game;
+
+import academy.mindswap.games.factory.Game;
+import academy.mindswap.games.factory.GameType;
+import academy.mindswap.games.factory.GamesFactory;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -20,10 +23,9 @@ import java.util.Optional;
 import java.util.concurrent.*;
 
 /**
- *Class responsible for creating the server that will start a game and listen to players
+ * Class responsible for creating the server that will start a game and listen to players
  * When a player arrives his socket will be sent to the available game
  * If a game is already full and not accepting players it creates a new game to accept new players.
- *
  */
 public class Server {
 
@@ -41,6 +43,7 @@ public class Server {
     /**
      * Main method of the class Server
      * Accepts a port as an argument. If no port is provided the default is 8080
+     *
      * @param args the port to start the server
      */
     public static void main(String[] args) {
@@ -49,7 +52,7 @@ public class Server {
 
         try {
             server.start(port);
-        }catch (RejectedExecutionException | IOException e){
+        } catch (RejectedExecutionException | IOException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -57,16 +60,17 @@ public class Server {
     /**
      * Parses the args given to main method.
      * Exits with error 1 if an illegal port number was provided as argument
+     *
      * @param args the arguments provided to main method
      * @return port 8080 if no port was provided to main, otherwise uses the provided port
      */
-    private int getServerPort(String[] args){
+    private int getServerPort(String[] args) {
         int port = 8080;
         try {
-            if (args.length>0) {
+            if (args.length > 0) {
                 port = Integer.parseInt(args[0]);
             }
-        }catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             System.out.println(NOT_VALID_ARGS);
             System.exit(1);
         }
@@ -77,9 +81,10 @@ public class Server {
      * Starts the server in the specified port
      * If there is no game available for players to join creates a new one
      * Waits for a client to connect to the server and add it to an available game
+     *
      * @param port the port that will accept client connections
      * @throws RejectedExecutionException the game thread couldn't start
-     * @throws IOException the server socket couldn't start
+     * @throws IOException                the server socket couldn't start
      */
     public void start(int port) throws RejectedExecutionException, IOException {
         ServerSocket serverSocket = new ServerSocket(port);
@@ -89,8 +94,9 @@ public class Server {
 
         while (serverSocket.isBound()) {
 
-            if (!isGameAvailable() ){
-                createGame();
+            if (!isGameAvailable()) {
+                //TODO to implement the methods to allow the player to choose the game
+                createGame(GameType.WHEEL_OF_FORTUNE);
                 System.out.println(NEW_GAME);
             }
 
@@ -98,11 +104,11 @@ public class Server {
                 getAvailableGame().get().acceptPlayer(serverSocket.accept());
                 System.out.println(PLAYER_ADDED_TO_GAME);
 
-              try{ //Give some time to start another game or an error will occur
-                   Thread.sleep(40);
-               }catch (InterruptedException e) {
-                  System.out.println(e.getMessage());
-               }
+                try { //Give some time to start another game or an error will occur
+                    Thread.sleep(40);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
 
             }
         }
@@ -111,24 +117,28 @@ public class Server {
     /**
      * Creates a new game and sends it to the execution service as a new thread
      * Adds the created game to the gameList of the server
+     *
      * @throws RejectedExecutionException the thread couldn't start
      */
-    private void createGame() throws RejectedExecutionException {
-        Game game=new Game(this);
+    private void createGame(GameType gameType) throws RejectedExecutionException {
+
+        Game game = GamesFactory.create(this, gameType);
         gameList.add(game);
         gamesService.execute(game);
     }
 
     /**
      * Goes through the gameList of the server and checks if there is a game that can accept a player
+     *
      * @return returns true if there is a game that can accept a player false if all games are full
      */
-    private synchronized boolean isGameAvailable(){
-       return gameList.stream().anyMatch(Game::isAcceptingPlayers);
+    private synchronized boolean isGameAvailable() {
+        return gameList.stream().anyMatch(Game::isAcceptingPlayers);
     }
 
     /**
      * Goes through the gameList of the server and returns an available game
+     *
      * @return returns the first available game that can accept a player
      */
     private synchronized Optional<Game> getAvailableGame() {
@@ -137,10 +147,11 @@ public class Server {
 
     /**
      * Removes a game from the gameList. This method is invoked by the game itself
+     *
      * @param game the game object to be removed.
      */
-    public synchronized void removeGameFromList(Game game){
-        if(gameList.contains(game)){
+    public synchronized void removeGameFromList(Game game) {
+        if (gameList.contains(game)) {
             gameList.remove(game);
             System.out.println(GAME_REMOVED);
         }
